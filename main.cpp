8afcfe91011a2360d3202b1fa8757a7c1b22b7cf
Mateus <57511045+Mateusilva08@users.noreply.github.com>
2022-09-09 21:48:12 -0300

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+using std::cout;
+
+#include "Sayajin.h"
+
+int main() {
+  cout << "Usando Sayajin no main\n";
+
+  Sayajin goku("Goku", "Normal", 8348);
+  Sayajin vegeta("vegeta", "Normal", 7924);
+
+  goku.print();
+  vegeta.print();
+
+  goku.transform();
+  goku.print();
+
+  vegeta.transform();
+  vegeta.print();
+
+  goku.transform();
+  goku.print();
+
+  goku.transform();
+  goku.print();
+
+  goku.untransform();
+  goku.print();
+  
+  vegeta.untransform();
+  vegeta.print();
+  
+  return 0;
+}
